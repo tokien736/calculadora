c75c4940008c627976a52f7d8b89dd782a0f90a7
Jose Enrique Flores Espinoza <72504407@continental.edu.pe>
2023-04-12 16:10:12 -0500

    Se agrega el modulo de sumar

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,14 @@
 class Calculadora:
-    def __init__(self, num1, num2, resultado ):
+    def __init__(self, num1, num2,  ):
         self.num1 = num1
         self.num2 = num2
-        self.result = resultado
+
     def __str__(self):
         return f"{self.num1} {self.num2} {self.result}"
-
-suma = Calculadora(1, 1 ,2)
-print(suma)
+        
+    def suma(self ):
+        self.result = self.num1 + self.num2
+        print('La suma es: ', self.result)
+        return 
+suma = Calculadora(1,2)
+suma.suma()
