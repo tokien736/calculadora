8f3a43072714d400f52a6d15517d8cc24396f269
Jose Enrique Flores Espinoza <72504407@continental.edu.pe>
2023-04-12 15:54:44 -0500

    Se crea la clase

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,10 @@
+class Calculadora:
+    def __init__(self, num1, num2, resultado ):
+        self.num1 = num1
+        self.num2 = num2
+        self.result = resultado
+    def __str__(self):
+        return f"{self.num1} {self.num2} {self.result}"
 
+suma = Calculadora(1, 1 ,2)
+print(suma)
