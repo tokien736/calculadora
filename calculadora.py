ca78c437d78898b861447f254b695c298cdd6315
Jose Enrique Flores Espinoza <72504407@continental.edu.pe>
2023-04-12 16:53:34 -0500

    Se agrega un menu y execepciones

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,5 @@
+from os import system
+
 class Calculadora:
     def __init__(self, num1, num2  ):
         self.num1 = num1
@@ -24,9 +26,93 @@ class Calculadora:
         self.result = self.num1 / self.num2
         print('La división es: ', self.result)
         return             
-prueba = Calculadora(4,2)
-prueba.suma()
-prueba.resta()
-prueba.multiplicacion()
-prueba.division()
+
+
+while True: 
+    try:
+        print("===================")
+        print("======MENÚ========")
+        print("===================")
+        print("1. Suma ")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+        print("5. Salir")
+
+
+        opc = int(input("Ingrese una opcion: "))
+        if opc == 1:
+            while True:
+                try:
+                    print("=====SUMA=====")
+                    x = int(input("Ingrese el primero numero: "))
+                    y = int(input("Ingrese el segundo numero: "))
+                    c = Calculadora(x, y) 
+                    c.suma()
+                    break
+                except ValueError:
+                    print("Por favor, ingrese solo numeros")
+                    respuesta = input("Desea volver a ingresar los datos? [s/n]: ")
+                    if respuesta == "n":
+                        break
+        elif opc == 2:
+            while True:
+                try:
+                    print("=====RESTA=====")
+                    x = int(input("Ingrese el primero numero: "))
+                    y = int(input("Ingrese el segundo numero: "))
+                    c = Calculadora(x, y) 
+                    c.resta()
+                    break
+                except ValueError:
+                    print("Por favor, ingrese solo numeros")
+                    respuesta = input("Desea volver a ingresar los datos? [s/n]: ")
+                    if respuesta == "n":
+                        break      
+        elif opc == 3:
+            while True:
+                try:
+                    print("=====MULTIPLICACIÓN=====")
+                    x = int(input("Ingrese el primero numero: "))
+                    y = int(input("Ingrese el segundo numero: "))
+                    c = Calculadora(x, y) 
+                    c.multiplicacion()
+                    break
+                except ValueError:
+                    print("Por favor, ingrese solo numeros")
+                    respuesta = input("Desea volver a ingresar los datos? [s/n]: ")
+                    if respuesta == "n":
+                        break       
+        elif opc == 4:
+            while True:
+                try:
+                    print("=====DIVISION=====")
+                    x = int(input("Ingrese el primero numero: "))
+                    y = int(input("Ingrese el segundo numero: "))
+                    c = Calculadora(x, y) 
+                    c.division()
+                    break
+                except ValueError:
+                    print("Por favor, ingrese solo numeros")
+                    respuesta = input("Desea volver a ingresar los datos? [s/n]: ")
+                    if respuesta == "n":
+                        break      
+                except ZeroDivisionError:
+                    print("ALERTA, ESTA DIVIDIENDO UN NUMERO ENTRE CERO")
+                    respuesta = input("Desea volver a ingresar los datos? [s/n]: ")
+                    if respuesta == "n":
+                        break     
+        elif opc == 5:
+            system("cls")
+            print("Gracias por Ingresar al Programa")
+            break                                                                   
+    except ValueError:
+        print("Por favor, ingrese solo numeros")
+        respuesta = input("Desea volver a ingresar los datos? [s/n]: ")
+        if respuesta == "n":
+            break
+     
+
+
+
 
